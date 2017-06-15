c9bbbc1f425820a6ec5c56f6b903acb457c48d11
linhanrui <Henry.lin@ubtrobot.com>
2017-06-15 20:39:04 +0800

    add some fields

--- expressMysql/dao/shopSqlMapping.js
+++ expressMysql/dao/shopSqlMapping.js
@@ -1,6 +1,6 @@
 var shop = {
-	insert: 'insert into xhs_product(buyable, category_id, description, discount_price, feature, height, id, image, ipq, link, new_arriving, price, promotion_text, seller_id , skucode, stock_shortage, stock_status , title, vendor_icon, whcode , width) '
-		+'values (?, ?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?, ?)',
+	insert: 'insert into xhs_product(buyable, category_id, category_name, description, discount_price, feature, height, id, image, ipq, link, new_arriving, price, promotion_text, seller_id , skucode, stock_shortage, stock_status , title, vendor_icon, whcode , width, created_time, is_deleted) '
+		+'values (?, ?, ?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?, ?, SYSDATE(), \'0\')',
 	update: 'update xhs_product set category_id=?, price=? where id=?',
 	delete: 'delete from xhs_product where id=?',
 	queryById: 'select * from xhs_product where id=?',
